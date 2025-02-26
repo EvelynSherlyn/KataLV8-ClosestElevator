d4683d43723ac67fbddb61b5d01dd3a9d7ac5ea3
Evelyn Sherlyn <evelynsan1805@gmail.com>
2025-02-26 00:27:20 +0000

    Third Commit: I learned about Math.abs(). Fixed

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,23 +1,16 @@
 public class Elevator {
 
     public static String call(int left, int right, int call) {
+        //Math.abs() Nos da el número absoluto. Por lo que, no da negativo y es perfecto para calcular distancia.
+        int izq= Math.abs(left-call);   int der=Math.abs(right-call);
         if (left == call && right != call) {
             return "left";
         } else if (right==call && left != call) {
             return "right";
-        }else if(call>right && call>left) {
-            if (right>left) {
-                return "right";
-            } else if (left>right) {
-                return "left";
-            }
-        } else if(call<right && call<left) {
-            if (right<left) {
-                return "right";
-            } else if (left<right) {
-                return "left";
-            }
+        }else if(izq<der){ //Si la distancia de la derecha al call es mayor que al de la izquierda, nos devolverá izquierda
+            return "left";
+        } else {
+            return "right";
         }
-        return "right";
     }
 }
